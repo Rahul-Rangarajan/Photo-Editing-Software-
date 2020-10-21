303ea7a270b8d0752d15aa2bde071490a5878ecc
Jack Castiglione <jhc.castiglione@gmail.com>
2020-10-20 21:32:15 -0500

    Initialize repository and create main.py python file

---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+Contributors:
+    Jack Castiglione
+    Rahul Rangarajan
+    Cameron King
+    Nick Jonas
+'''
+
+import numpy as np
+from PIL import Image
+import tkinter as tk
+import os
+
+
+def main():
+    print("Hi")
+
+if __name__ == "__main__":
+    main()
